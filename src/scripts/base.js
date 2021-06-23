14396ba263d6e9ee31f47c1f13d4a87c9755c0a1
PurpleHobo <cttrybula@gmail.com>
2021-06-23 15:49:39 +1000

    fixed some awful code

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,4 +1,37 @@
 //base holds general funcs redirects too other pages 
-function go(redir){
+//
+var go = function (redir){
     location.href = redir;
 }
+
+//loads vars after DOM content to avoid globals
+document.addEventListener('DOMContentLoaded', function() {
+    var goalRedir = document.getElementById("goGoals");
+    var mainRedir = document.getElementById("goMain");
+    var taskRedir = document.getElementById('goTask');
+    var gameRedir = document.getElementById('goGame');
+
+    if (mainRedir != null) {
+        mainRedir.onclick = function(){
+            go("./../html/index.html");
+        }
+    }
+
+    if (goalRedir != null) {
+        goalRedir.onclick = function(){
+            go("./../html/goals.html");
+        }
+    }
+
+    if (taskRedir != null) {
+        taskRedir.onclick = function(){
+            go("./../html/task.html");
+        }
+    }
+
+    if (gameRedir != null) {
+        gameRedir.onclick = function(){
+            go("./../html/game.html");
+        }
+    }
+});
